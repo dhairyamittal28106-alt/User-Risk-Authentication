6d15b763d23c6e6fe5aa9f620903b7870e1ff8e2
grushamittal05 <grushamittal05@gmail.com>
2026-07-02 22:52:40 +0530

    Updated data cleaning script and requirements

---
+++ src/data_cleaning.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly as py
+import pandas as pd
+
+df = pd.read_csv("src/CICIDS2017.csv")
+print(df.shape)
