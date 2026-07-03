021e306a90cc6bd6560f68670fda9e2f32272bf1
dhairyamittal28106-alt <dhairyamittal28106@gmail.com>
2026-07-03 10:39:09 +0530

    Removed

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,11 +4,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-df=pd.read_csv("src/CICIDS2017.csv")
+df=pd.read_csv("src/auth_authz_failures_dataset.csv")
 print(df.head(3))
 df.info()
 print(df.isnull().sum())
 print("Duplicate rows:", df.duplicated().sum())
-
+df=df.drop("error_code", axis=1)
+df.head()
 print("Infinite values:", np.isinf(df.select_dtypes(include=[np.number])).sum().sum())
-df.to_csv("src/CICIDS2017_cleaned.csv", index=False)
+df.to_csv("src/auth_authz_failures_dataset_cleaned.csv", index=False)
+print(df.columns)
