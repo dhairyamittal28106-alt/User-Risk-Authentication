13bd7c83a62a8028a3938e0d056602226eb80ec5
Ayush Parmar <ayushparmar1708@gmail.com>
2026-07-04 14:02:48 +0530

    Updated data cleaning file

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,11 +4,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-df=pd.read_csv("src/CICIDS2017.csv")
-print(df.head(3))
+df=pd.read_csv("src/auth_authz_failures_dataset.csv")
+print(df.shape)
 df.info()
-print(df.isnull().sum())
-print("Duplicate rows:", df.duplicated().sum())
+print("Rows:",df.shape[0])
 
+print("Columns:",df.shape[1])
+
+print(df.isnull().sum())
+df[df.isnull().any(axis=1)]
+df["failure_reason"] = df["failure_reason"].fillna("None")
+df["error_code"] = df["error_code"].fillna("None")
+
+print("Duplicate rows:", df.duplicated().sum())
+df=df.drop("error_code", axis=1)
+df.head()
 print("Infinite values:", np.isinf(df.select_dtypes(include=[np.number])).sum().sum())
-df.to_csv("src/CICIDS2017_cleaned.csv", index=False)
+
+
+
+df.to_csv("src/auth_authz_failures_dataset_cleaned.csv", index=False)
